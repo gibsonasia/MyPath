3610315268664d769900a908a7a1a2ad421fda2c
Nesada <knesada@yahoo.com>
2017-02-18 17:21:57 -0500

    Updated Five Boroughs

---
+++ app/src/main/java/com/example/asiagibson/mypath/network/SchoolsResponse.java
@@ -0,0 +1,19 @@
+package com.example.asiagibson.mypath.network;
+
+import com.example.asiagibson.mypath.models.Schools;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+
+/**
+ * Created by Nesada Koca on 2/18/2017.
+ */
+
+public interface SchoolsResponse {
+
+    @GET("resource/jfsi-vq8f.json")
+    Call<List<Schools>> schoolsData();
+}
