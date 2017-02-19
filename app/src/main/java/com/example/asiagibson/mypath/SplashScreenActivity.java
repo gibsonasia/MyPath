3ab6d6ee68534f8885fe7c6c6d9b4b6cd51c80bd
wesniem <wesniem@yahoo.com>
2017-02-19 12:33:08 -0500

    Added Changes to UI

---
+++ app/src/main/java/com/example/asiagibson/mypath/SplashScreenActivity.java
@@ -0,0 +1,37 @@
+package com.example.asiagibson.mypath;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_activity);
+
+
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.anim_hyperspace_jump);
+        ImageView imageView = (ImageView) findViewById(R.id.path_icon);
+        imageView.setAnimation(animation);
+
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+                finish();
+//
+            }
+        }, 10000);
+
+
+
+    }
+}
