d9c31686ba5ddb7c2d905a2fa8c0970e04d4e434
Asia <gibsonjasia@gmail.com>
2017-02-21 09:07:30 -0500

    synced style

--- app/src/main/java/com/example/asiagibson/mypath/rv/EsllViewHolder.java
+++ app/src/main/java/com/example/asiagibson/mypath/rv/EsllViewHolder.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.example.asiagibson.mypath.R;
@@ -13,9 +14,10 @@ import com.example.asiagibson.mypath.model.EsllData;
  * Created by asiagibson on 2/18/17.
  */
 
-public class EsllViewHolder extends RecyclerView.ViewHolder {
+public class EsllViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
-    TextView tv1,tv2,tv3,tv4;
+    ImageButton mBtnMap;
+    TextView tv1, tv2, tv3, tv4;
 
     public EsllViewHolder(View view) {
         super(view);
@@ -24,26 +26,26 @@ public class EsllViewHolder extends RecyclerView.ViewHolder {
         tv2 = (TextView) itemView.findViewById(R.id.esll_tv2);
         tv3 = (TextView) itemView.findViewById(R.id.esll_tv3);
         tv4 = (TextView) itemView.findViewById(R.id.esll_tv4);
+        mBtnMap = (ImageButton) itemView.findViewById(R.id.btn_map);
+        mBtnMap.setOnClickListener(this);
+
 
     }
 
     public void bind(final EsllData esllData) {
         tv1.setText(esllData.getSite_name());
         tv2.setText(esllData.getLocation_1_location());
-
-        tv2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String formattedAddress = "geo:0,0?q=" + esllData.getLocation_1_location().replaceAll(" ", "+");
-                Uri uri = Uri.parse(formattedAddress);
-                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                view.getContext().startActivity(intent);
-            }
-        });
-
         tv3.setText(esllData.getContact_number());
         tv4.setText(esllData.getProgram());
 
 
+    }
+
+    public void onClick(View view) {
+
+        Intent intent = new Intent(android.content.Intent.ACTION_VIEW,
+                Uri.parse("http://maps.google.co.in/maps?q=" + tv2.getText() + tv1.getText()));
+
+        view.getContext().startActivity(intent);
     }
 }
--- app/src/main/java/com/example/asiagibson/mypath/rv/YaipViewHolder.java
+++ app/src/main/java/com/example/asiagibson/mypath/rv/YaipViewHolder.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,9 +15,10 @@ import com.example.asiagibson.mypath.model.Location;
  * Created by asiagibson on 2/18/17.
  */
 
-public class YaipViewHolder extends RecyclerView.ViewHolder {
+public class YaipViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
-    TextView textView,mTv2, mTv3;
+     ImageButton mBtnMap;
+    TextView textView, mTv2, mTv3;
 
 
     public YaipViewHolder(View view) {
@@ -24,22 +26,26 @@ public class YaipViewHolder extends RecyclerView.ViewHolder {
         textView = (TextView) itemView.findViewById(R.id.yaip_tv);
         mTv2 = (TextView) itemView.findViewById(R.id.tv2);
         mTv3 = (TextView) itemView.findViewById(R.id.tv3);
+        mBtnMap = (ImageButton) itemView.findViewById(R.id.btn_map);
+
+        mBtnMap.setOnClickListener(this);
 
     }
+
     public void bind(final Location location) {
 
-        textView.setText("Program type"+ location.getProgram_type());
+        textView.setText("Program type" + location.getProgram_type());
         mTv2.setText(location.getContact_number());
         mTv3.setText(location.getLocation_1_location());
-        mTv3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String formattedAddress = "geo:0,0?q=" + location.getLocation_1_location().replaceAll(" ", "+");
-                Uri uri = Uri.parse(formattedAddress);
-                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                view.getContext().startActivity(intent);
-            }
-        });
 
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        Intent intent = new Intent(android.content.Intent.ACTION_VIEW,
+                Uri.parse("http://maps.google.co.in/maps?q=" + mTv3.getText()));
+
+        view.getContext().startActivity(intent);
     }
 }
